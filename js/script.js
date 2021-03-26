e54533f3f40cfbaa02b452e9afdc56e827dc254d
Tommaso P <tommaso.plinio@gmail.com>
2021-03-26 17:27:32 +0100

    todo list custom

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+
+
+
 var app = new Vue ({
 
   el: "#todo-list",
   data : {
     newItem : "",
-    lista : ['vino', 'pane', 'cioccolata', 'latte', 'birra']
+    lista : [
+      'vino', 'pane', 'cioccolata', 'latte', 'birra'
+    ]
   },
   methods: {
     deleteItem : function(itemIndex){
